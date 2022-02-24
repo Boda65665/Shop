26cd42701624ec6303501f3dd42789bf57b3a55f
Boda65665 <pasaharpsuk@gmail.com>
2022-02-24 08:09:01 +0300

    create ShopController

--- src/main/java/ru/shop/com/Conrollers/ShopControllers.java
+++ src/main/java/ru/shop/com/Conrollers/ShopControllers.java
@@ -1,4 +1,19 @@
 package ru.shop.com.Conrollers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.shop.com.DAO.UserDAO;
+import ru.shop.com.email.Mail;
+
+@Controller
+@RequestMapping("/shop")
 public class ShopControllers {
+    private final UserDAO userDAO;
+
+    @Autowired
+    public ShopControllers(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
 }
